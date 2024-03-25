111fe835ab6a40425ded7af5a67d723185dc61ca
VZachary13 <vinicius.zachary@gmail.com>
2024-03-25 13:53:28 -0700

    refactored view commands to properly format query into tables with joined table information

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const inquirer = require('inquirer');
 const express = require('express');
 const mysql = require('mysql2');
-const {question1, addDept} = require('./lib/questions');
+const { table } = require('table');
+const {question1, addDept, addRole} = require('./lib/questions');
 require('dotenv').config();
 
-const PORT = 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
@@ -23,30 +23,58 @@ const db = mysql.createConnection(
 inquirer.prompt(question1).then((data) => {
     switch (data.choice) {
         case 'View all departments':
-            db.query('SELECT * FROM departments', (error, results) => {
+            db.query('SELECT id, dptname AS name FROM departments', (error, results) => {
                 if (error) {
                   console.error('Error executing query:', error);
                   return;
                 } 
-                console.log(results)
+                const newTable = [
+                    ['Department ID', 'Department Name']
+                ] 
+
+                results.forEach(department => {
+                    newTable.push([department.id, department.name])
+                })
+
+                console.log(table(newTable));
             })
             break;
         case 'View all roles':
-            db.query('SELECT * FROM roles', (error, results) => {
+            db.query(`SELECT roles.id, roles.title, roles.salary, departments.dptname AS department FROM roles 
+            INNER JOIN departments ON roles.department_id = departments.id`, (error, results) => {
                 if (error) {
                   console.error('Error executing query:', error);
                   return;
-                } 
-                console.log(results)
+                }  
+                const newTable = [
+                    ['Role ID', 'Title', 'Salary', 'Department']
+                ] 
+
+                results.forEach(role => {
+                    newTable.push([role.id, role.title, role.salary, role.department])
+                })
+
+                console.log(table(newTable));
             })
             break;
         case 'View all employees':
-            db.query('SELECT * FROM employees', (error, results) => {
+            db.query(`SELECT employees.id, CONCAT(employees.first_name, ' ', employees.last_name) AS name, roles.title, roles.salary, departments.dptname AS department, CONCAT(managers.first_name, ' ', managers.last_name) AS manager FROM employees
+            INNER JOIN roles ON employees.role_id = roles.id
+            INNER JOIN departments ON roles.department_id = departments.id
+            LEFT JOIN employees AS managers ON employees.manager_id = managers.id`, (error, results) => {
                 if (error) {
                   console.error('Error executing query:', error);
                   return;
-                } 
-                console.log(results)
+                }  
+                const newTable = [
+                    ['Employee ID', 'Name', 'Title', 'Salary', 'Department', 'Manager']
+                ] 
+
+                results.forEach(employee => {
+                    newTable.push([employee.id, employee.name, employee.title, employee.salary, employee.department, employee.manager])
+                })
+
+                console.log(table(newTable));
             })
             break;
         case 'Add a department':
@@ -61,12 +89,31 @@ inquirer.prompt(question1).then((data) => {
             })
             break;
         case 'Add a role':
-            db.query('SELECT * FROM roles', (error, results) => {
-                if (error) {
-                  console.error('Error executing query:', error);
-                  return;
-                } 
-                console.log(results)
+            inquirer.prompt(addRole).then((data) => {
+                var deptIDQuery;
+                db.query('SELECT id, dptname FROM departments', (error, results) => {
+                    if (error) {
+                      console.error('Error fetching department IDs:', error);
+                      return;
+                    } 
+                    deptIDQuery = results.map(department => `${department.id}: ${department.dptname}`);
+                    inquirer.prompt({
+                        type: 'list',
+                        name: 'dptid',
+                        message: 'Please select from the available department IDs',
+                        choices: deptIDQuery
+                    }).then((data2) => {
+                        const newData = data2.dptid;
+                        const dptid = newData.split(':');
+                        db.query('INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)', [data.title, data.salary, dptid[0]], (error, results) => {
+                            if (error) {
+                                console.error('Error adding department:', error);
+                                return;
+                            }
+                            console.log('Role added successfully.');             
+                        })
+                    })
+                })
             })
             break;
         case 'Add an employee':
